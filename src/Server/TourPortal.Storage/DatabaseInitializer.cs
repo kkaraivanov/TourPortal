a067bfa220a382d2af4b277383c9f161e7bcd067
kkaraivanov <kostadin.karaivanov@outlook.com>
2021-06-28 15:52:30 +0300

    add database initializer

---
+++ src/Server/TourPortal.Storage/DatabaseInitializer.cs
@@ -0,0 +1,13 @@
+﻿namespace TourPortal.Storage
+{
+    using System.Threading.Tasks;
+    using Infrastructure.Storage;
+
+    public class DatabaseInitializer : IDatabaseInitializer
+    {
+        public Task InitializeAsync()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
