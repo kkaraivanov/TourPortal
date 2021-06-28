002d2dcb949784d150427ecac4c353bccd34efd7
kkaraivanov <kostadin.karaivanov@outlook.com>
2021-06-28 15:55:52 +0300

    complete class database initializer

--- src/Server/TourPortal.Storage/DatabaseInitializer.cs
+++ src/Server/TourPortal.Storage/DatabaseInitializer.cs
@@ -1,13 +1,42 @@
 ﻿namespace TourPortal.Storage
 {
     using System.Threading.Tasks;
+    using Microsoft.AspNetCore.Identity;
+    using Microsoft.EntityFrameworkCore;
+    using Microsoft.Extensions.Logging;
+
     using Infrastructure.Storage;
+    using Infrastructure.Storage.Models;
 
     public class DatabaseInitializer : IDatabaseInitializer
     {
-        public Task InitializeAsync()
+        private readonly ApplicationDbContext _context;
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly RoleManager<ApplicationRole> _roleManager;
+        private readonly ILogger<DatabaseInitializer> _logger;
+
+        public DatabaseInitializer(
+            ApplicationDbContext context,
+            UserManager<ApplicationUser> userManager,
+            RoleManager<ApplicationRole> roleManager,
+            ILogger<DatabaseInitializer> logger)
         {
-            throw new System.NotImplementedException();
+            _context = context;
+            _userManager = userManager;
+            _roleManager = roleManager;
+            _logger = logger;
         }
+
+        public async Task InitializeAsync()
+        {
+            await Migrate();
+        }
+
+        private async Task Migrate()
+        {
+            await _context.Database.MigrateAsync();
+        }
+
+        // TODO: set _userManager and _roleManager. For record error i'm used _logger
     }
 }
