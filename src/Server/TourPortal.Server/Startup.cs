72291de635844c89f911cb362e536d31b6290fc9
kkaraivanov <kostadin.karaivanov@outlook.com>
2021-06-28 18:45:06 +0300

    add jwt token on the services

--- src/Server/TourPortal.Server/Startup.cs
+++ src/Server/TourPortal.Server/Startup.cs
@@ -1,7 +1,10 @@
 namespace TourPortal.Server
 {
+    using System;
+    using System.Text;
     using Infrastructure.Storage;
     using Infrastructure.Storage.Models;
+    using Microsoft.AspNetCore.Authentication.JwtBearer;
     using Microsoft.AspNetCore.Builder;
     using Microsoft.AspNetCore.Hosting;
     using Microsoft.AspNetCore.Http;
@@ -10,6 +13,7 @@ namespace TourPortal.Server
     using Microsoft.Extensions.Configuration;
     using Microsoft.Extensions.DependencyInjection;
     using Microsoft.Extensions.Hosting;
+    using Microsoft.IdentityModel.Tokens;
     using Storage;
 
     public class Startup
@@ -26,6 +30,8 @@ namespace TourPortal.Server
 
         public void ConfigureServices(IServiceCollection services)
         {
+            var jwtKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(Configuration["JwtSecurity:JwtSecurityKey"]));
+            
             services.AddStorageServices(Configuration);
             services.Configure<ApiBehaviorOptions>(options => { options.SuppressModelStateInvalidFilter = true; });
             services
@@ -33,6 +39,21 @@ namespace TourPortal.Server
                 .AddRoles<ApplicationRole>()
                 .AddEntityFrameworkStores<ApplicationDbContext>()
                 .AddDefaultTokenProviders();
+
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                .AddJwtBearer(options =>
+                {
+                    options.TokenValidationParameters = new TokenValidationParameters
+                    {
+                        ValidateIssuer = true,
+                        ValidateAudience = true,
+                        ValidateLifetime = true,
+                        ValidateIssuerSigningKey = true,
+                        ValidIssuer = Configuration["JwtSecurity:JwtIssuer"],
+                        ValidAudience = Configuration["JwtSecurity:JwtAudience"],
+                        IssuerSigningKey = jwtKey
+                    };
+                });
         }
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
