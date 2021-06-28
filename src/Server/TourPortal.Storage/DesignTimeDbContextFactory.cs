1ace65f9523b0ce5308d8913cbe80a0e457aabac
kkaraivanov <kostadin.karaivanov@outlook.com>
2021-06-28 15:43:33 +0300

    add class DesignTimeDbContextFactory

---
+++ src/Server/TourPortal.Storage/DesignTimeDbContextFactory.cs
@@ -0,0 +1,13 @@
+﻿namespace TourPortal.Storage
+{
+    using Microsoft.EntityFrameworkCore.Design;
+
+    // used documentation https://docs.microsoft.com/en-us/ef/core/cli/dbcontext-creation?tabs=dotnet-core-cli
+    public class DesignTimeDbContextFactory : IDesignTimeDbContextFactory<ApplicationDbContext>
+    {
+        public ApplicationDbContext CreateDbContext(string[] args)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
