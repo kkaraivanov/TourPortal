3d8ce3f9678c3e18942a1e2bf06f994cd3ddc4e8
kkaraivanov <kostadin.karaivanov@outlook.com>
2021-06-28 15:49:06 +0300

    add logic to DesignTimeDbContextFactory

--- src/Server/TourPortal.Storage/DesignTimeDbContextFactory.cs
+++ src/Server/TourPortal.Storage/DesignTimeDbContextFactory.cs
@@ -1,13 +1,24 @@
 ﻿namespace TourPortal.Storage
 {
+    using System.IO;
+    using Microsoft.EntityFrameworkCore;
     using Microsoft.EntityFrameworkCore.Design;
+    using Microsoft.Extensions.Configuration;
 
     // used documentation https://docs.microsoft.com/en-us/ef/core/cli/dbcontext-creation?tabs=dotnet-core-cli
     public class DesignTimeDbContextFactory : IDesignTimeDbContextFactory<ApplicationDbContext>
     {
         public ApplicationDbContext CreateDbContext(string[] args)
         {
-            throw new System.NotImplementedException();
+            var configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true).Build();
+
+            var builder = new DbContextOptionsBuilder<ApplicationDbContext>();
+            var connectionString = configuration.GetConnectionString("DefaultConnection");
+            builder.UseSqlServer(connectionString);
+
+            return new ApplicationDbContext(builder.Options);
         }
     }
 }
