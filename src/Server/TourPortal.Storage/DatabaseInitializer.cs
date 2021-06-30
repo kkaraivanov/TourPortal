2a3007d2fea2d90937e276093d0811ae4cdb0f96
kkaraivanov <kostadin.karaivanov@outlook.com>
2021-06-30 20:11:59 +0300

    add logger information

--- src/Server/TourPortal.Storage/DatabaseInitializer.cs
+++ src/Server/TourPortal.Storage/DatabaseInitializer.cs
@@ -44,44 +44,56 @@
             await _context.Database.MigrateAsync();
         }
 
-        // TODO: set _userManager and _roleManager. For record error i'm used _logger
         private async Task SeedApplicationUser()
         {
             var roles = await _roleManager.FindByNameAsync(Security.Role.Administrator);
 
             if (roles == null)
             {
+                _logger.LogError("The following role types are invalid: ");
                 throw new Exception("The following role types are invalid: ");
             }
 
-            if (await _userManager.FindByNameAsync(ApplicationConstants.DefaultAdministratorName) == null)
+            try
             {
-                await CreateUser(
-                    ApplicationConstants.DefaultAdministratorName,
-                    ApplicationConstants.DefaultAdministratorPassword,
-                    "Application",
-                    "",
-                    "Administrator",
-                    "admin@test.com",
-                    "555 555 555",
-                    new string[]{roles.ToString()});
-            }
-
-            var user = await _userManager.FindByNameAsync(ApplicationConstants.DefaultAdministratorName);
-            var userProfile = await _context.UserProfiles.FirstOrDefaultAsync(x => x.UserId == user.Id);
-
-            if (userProfile == null)
-            {
-                _context.UserProfiles.Add(new UserProfile
+                if (await _userManager.FindByNameAsync(ApplicationConstants.DefaultAdministratorName) == null)
                 {
-                    UserId = user.Id,
-                    ApplicationUser = user,
-                    CreatedOn = DateTime.Now,
-                    ModifiedOn = DateTime.Now,
-                });
-            }
+                    await CreateUser(
+                        ApplicationConstants.DefaultAdministratorName,
+                        ApplicationConstants.DefaultAdministratorPassword,
+                        "Application",
+                        "",
+                        "Administrator",
+                        "admin@test.com",
+                        "555 555 555",
+                        new string[]{roles.ToString()});
+                }
+                else
+                {
+                    _logger.LogInformation("User {0} exist in db. Seed to database return.", ApplicationConstants.DefaultAdministratorName);
+                    return;
+                }
 
-            _context.SaveChanges();
+                var user = await _userManager.FindByNameAsync(ApplicationConstants.DefaultAdministratorName);
+                var userProfile = await _context.UserProfiles.FirstOrDefaultAsync(x => x.UserId == user.Id);
+
+                if (userProfile == null)
+                {
+                    _context.UserProfiles.Add(new UserProfile
+                    {
+                        UserId = user.Id,
+                        ApplicationUser = user,
+                        CreatedOn = DateTime.Now,
+                        ModifiedOn = DateTime.Now,
+                    });
+                }
+
+                _context.SaveChanges();
+            }
+            catch (Exception e)
+            {
+                _logger.LogError("Importing administrator in db error: {0}", e.GetBaseException().Message);
+            }
         }
 
         private async Task<ApplicationUser> CreateUser(
@@ -128,7 +140,7 @@
                 }
 
                 ApplicationUser user = await _userManager.FindByNameAsync(applicationUser.UserName);
-
+                
                 try
                 {
                     result = await _userManager.AddToRolesAsync(user, roles.Distinct());
@@ -136,8 +148,27 @@
                 catch
                 {
                     await _userManager.DeleteAsync(user);
+                    _logger.LogError("Importing user in db error: {0} User {1} has deleted.", Environment.NewLine, user.UserName);
                     throw;
                 }
+
+                var userRoles = await _userManager.GetRolesAsync(user);
+                var claims = await _userManager.GetClaimsAsync(user);
+                var index = 1;
+
+                _logger.LogInformation("Add user {0} to roles: {1}{2}",
+                    user.UserName, Environment.NewLine, string.Join(
+                        Environment.NewLine,
+                        userRoles.Select(r =>
+                            $"\t ({index++}) {r}")));
+
+                index = 1;
+
+                _logger.LogInformation("Added Claims for user: {0}{1}{2}",
+                    user.UserName, Environment.NewLine, string.Join(
+                        Environment.NewLine,
+                        claims.Select(c =>
+                            $"\t ({index++}) {c.Type} - {c.Value}")));
 
                 if (!result.Succeeded)
                 {
@@ -145,6 +176,7 @@
                 }
             }
 
+            _logger.LogInformation("Importing {0} in db has completed", applicationUser.UserName);
             return applicationUser;
         }
     }
