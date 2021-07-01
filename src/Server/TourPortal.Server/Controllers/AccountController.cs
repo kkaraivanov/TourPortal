7b09f4190420080cd416232f50b717ab3a641140
kkaraivanov <kostadin.karaivanov@outlook.com>
2021-07-01 10:47:59 +0300

    policies and roles work

--- src/Server/TourPortal.Server/Controllers/AccountController.cs
+++ src/Server/TourPortal.Server/Controllers/AccountController.cs
@@ -1,31 +1,24 @@
 ﻿namespace TourPortal.Server.Controllers
 {
-    using System.ComponentModel.DataAnnotations;
     using System.Threading.Tasks;
     using Microsoft.AspNetCore.Authentication.JwtBearer;
     using Microsoft.AspNetCore.Authorization;
     using Microsoft.AspNetCore.Mvc;
 
     [Route("api/[controller]")]
+    [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
     public class AccountController : Controller
     {
         [HttpGet]
         [Route("[action]")]
-        [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
+        [Authorize(Roles = "Administrator")]
+        //[Authorize(Roles = "Employe")]
+        //[Authorize(Policy = "IsUser")]
+        //[Authorize(Policy = "IsAdministrator")]
         public async Task<IActionResult> TestTocken()
         {
             return Ok("Hallo from account controller");
         }
     }
-
-    public class LoginModel
-    {
-        [Required]
-        public string Email { get; set; }
-
-        [Required]
-        public string Password { get; set; }
-
-        public bool RememberMe { get; set; }
-    }
+    
 }
