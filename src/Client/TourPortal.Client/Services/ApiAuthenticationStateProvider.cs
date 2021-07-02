f977a264553c3144cad742d04da8f044e683e4e3
kkaraivanov <kostadin.karaivanov@outlook.com>
2021-07-02 17:02:07 +0300

    add AuthenticationStateProvider class

---
+++ src/Client/TourPortal.Client/Services/ApiAuthenticationStateProvider.cs
@@ -0,0 +1,13 @@
+﻿namespace TourPortal.Client.Services
+{
+    using System.Threading.Tasks;
+    using Microsoft.AspNetCore.Components.Authorization;
+
+    public class ApiAuthenticationStateProvider : AuthenticationStateProvider
+    {
+        public override Task<AuthenticationState> GetAuthenticationStateAsync()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
