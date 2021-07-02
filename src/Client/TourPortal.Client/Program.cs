6daee3bfeb3fdd8beed06486c9ab260c144ab15f
kkaraivanov <kostadin.karaivanov@outlook.com>
2021-07-02 16:18:59 +0300

    add first file on client side project

--- src/Client/TourPortal.Client/Program.cs
+++ src/Client/TourPortal.Client/Program.cs
@@ -1,10 +1,29 @@
 namespace TourPortal.Client
 {
+    using System;
+    using System.Net.Http;
+    using System.Threading.Tasks;
+    using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+    using Microsoft.Extensions.DependencyInjection;
+
     public class Program
     {
-        public static void Main(string[] args)
+        public static async Task Main(string[] args)
         {
+            var builder = WebAssemblyHostBuilder.CreateDefault(args);
+            builder.RootComponents.Add<App>("app");
+
+            builder.Services
+                .AddScoped(sp =>
+                new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) })
+                .AddSingleton(async sp => 
+                {
+                var httpClient = sp.GetRequiredService<HttpClient>();
+                return await httpClient.GetStringAsync("appsettings.json")
+                    .ConfigureAwait(false);
+                });
             
+            await builder.Build().RunAsync();
         }
     }
 }
