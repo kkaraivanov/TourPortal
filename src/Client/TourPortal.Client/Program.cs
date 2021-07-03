72ec68e8fb12221fd56adecd56d683b612ff178d
kkaraivanov <kostadin.karaivanov@outlook.com>
2021-07-03 18:12:51 +0300

    added files layout and index

--- src/Client/TourPortal.Client/Program.cs
+++ src/Client/TourPortal.Client/Program.cs
@@ -3,8 +3,10 @@ namespace TourPortal.Client
     using System;
     using System.Net.Http;
     using System.Threading.Tasks;
+    using Microsoft.AspNetCore.Components.Authorization;
     using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
     using Microsoft.Extensions.DependencyInjection;
+    using Services;
 
     public class Program
     {
@@ -14,15 +16,12 @@ namespace TourPortal.Client
             builder.RootComponents.Add<App>("app");
 
             builder.Services
-                .AddScoped(sp =>
-                new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) })
-                .AddSingleton(async sp =>
-                {
-                    var httpClient = sp.GetRequiredService<HttpClient>();
-                    return await httpClient
-                        .GetStringAsync("appsettings.json")
-                        .ConfigureAwait(false);
-                });
+                .AddSingleton(new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+            builder.Services
+                .AddAuthenticationCore()
+                .AddScoped<AuthenticationStateProvider, ApiAuthenticationStateProvider>()
+                .AddScoped<IAuthenticationService, AuthenticationService>();
 
             await builder.Build().RunAsync();
         }
