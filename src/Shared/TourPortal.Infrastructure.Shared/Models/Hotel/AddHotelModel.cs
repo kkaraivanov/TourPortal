936ef9ccd0d931eff531c2a3f593ed47685ed35f
kkaraivanov <kostadin.karaivanov@outlook.com>
2021-07-24 13:42:53 +0300

    add owner hotel card

--- src/Shared/TourPortal.Infrastructure.Shared/Models/Hotel/AddHotelModel.cs
+++ src/Shared/TourPortal.Infrastructure.Shared/Models/Hotel/AddHotelModel.cs
@@ -1,6 +1,8 @@
 ﻿namespace TourPortal.Infrastructure.Shared.Models.Hotel
 {
+    using System.Collections.Generic;
     using System.ComponentModel.DataAnnotations;
+    using Enums;
 
     public class AddHotelModel
     {
@@ -10,11 +12,13 @@
 
         [Required]
         [StringLength(20, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 3)]
-        public string Sity { get; set; }
+        public string City { get; set; }
 
         [Required]
         [StringLength(64, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 3)]
         public string Address { get; set; }
+
+        public Dictionary<string, ContactType> Contacts { get; set; } = new Dictionary<string, ContactType>();
 
         public string HotelImageUrl { get; set; }
     }
